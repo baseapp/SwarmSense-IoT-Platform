b72b6614ea16ad99ff03c5449ce2511c710743e3
Gopal Kildoliya <gopalkildoliya@gmail.com>
2018-12-13 15:47:20 +0530

    Filters added for normal request

--- snms/utils/__init__.py
+++ snms/utils/__init__.py
@@ -30,23 +30,37 @@ def get_filters(in_request, sort_by=None, order_type="ASC", limit=10000):
     filters = {}
     try:
         parser = reqparse.RequestParser()
+        parser.add_argument("order_by", type=str, location='args')
+        parser.add_argument("order", type=str, location='args')
         parser.add_argument("sort", type=str, location='args')
         parser.add_argument("filter", type=str, location='args', default="{}")
         parser.add_argument("range", type=str, location='args')
         args = parser.parse_args(req=in_request)
         print(args)
         if args['sort']:
-            order_by = json.loads(args['sort'])[0]
-            order_type = json.loads(args['sort'])[1]
-            if order_type not in ['ASC', 'DESC']:
-                order_type = 'DESC'
-            # NOTE: Postgres Specific NULLS LAST (http://docs.sqlalchemy.org/en/latest/core/sqlelement.html#sqlalchemy.sql.expression.nullslast).
-            if order_type in ['desc', 'DESC']:
-                order_type = 'DESC NULLS LAST'
+            sort = args['sort']
+            if sort[0] in ['-', '+']:
+                order_by = sort[1:]
+                if sort[0] == '+':
+                    order_type = "ASC"
+                else:
+                    order_type = "DESC"
+            else:
+                order_by = json.loads(args['sort'])[0]
+                order_type = json.loads(args['sort'])[1]
+                if order_type not in ['ASC', 'DESC']:
+                    order_type = 'DESC'
+                # NOTE: Postgres Specific NULLS LAST (http://docs.sqlalchemy.org/en/latest/core/sqlelement.html#sqlalchemy.sql.expression.nullslast).
+                if order_type in ['desc', 'DESC']:
+                    order_type = 'DESC NULLS LAST'
         if args['range']:
             offset = json.loads(args['range'])[0]
             end = json.loads(args['range'])[1]
             limit = end+1-offset
+        else:
+            limit = int(in_request.args.get('limit', limit))
+            page = int(in_request.args.get('page', 1))
+            offset = (page - 1) * limit
         filters = json.loads(args['filter'])
     except Exception as e:
         # TODO: Add logging.
